5a1320bacd97a1eddda4b9bd49781f1df5893ae8
Antonella Lundwall antonella.lundwall@chello.at <cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh>
2026-09-08 10:20:46 +1000

    Add file upload section

--- app.py
+++ app.py
@@ -9,3 +9,12 @@ st.set_page_config(
 st.title("🦟 Nobite Analytics")
 st.write("Upload your reports and turn them into insights.")
 st.divider()
+
+col1, col2 = st.columns(2)
+with col1:
+    st.header("Umsatzstatistik & PLZ")
+    sales_files = st.file_uploader("Sales Files hochladen", type=["xlsx"], accept_multiple_files=True)
+
+with col2:
+    st.header("Lagerliste")
+    inventory_files = st.file_uploader("Inventory files hochladen", type=["xlsx"], accept_multiple_files=True)
